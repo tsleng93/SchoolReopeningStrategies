d6a9c1d1708fa25a204e4be2610bd4814c67da44
tsleng93 <51326999+tsleng93@users.noreply.github.com>
2022-01-24 16:45:44 +0000

    Add files via upload

---
+++ Interactingyeargroups_externalinfection.m
@@ -0,0 +1,48 @@
+function [histInfnextday, histDayInf, histRday, histExtorIntnextday, will_take_PCR] = Interactingyeargroups_externalinfection(SusIndiv,  histDayInf,  Symptomatic, will_take_PCR, day_of_PCR, histIsol, histRday, r_infext, TodayExt, Ext_isolate, day)
+      
+
+histInfnextday = zeros(size(histIsol));
+histExtorIntnextday = zeros(size(histIsol));
+
+
+
+for ii = 1:length(SusIndiv)
+    
+    
+    student = SusIndiv(ii);
+    
+
+    
+     if histIsol(student) == 0
+         if r_infext(ii) < TodayExt
+             histInfnextday(student) = 1;
+             histDayInf(student) = day;
+             histRday(1,day) = histRday(1,day)+1;
+
+             histExtorIntnextday(student) = 1;
+
+
+             if Symptomatic(student)
+                  %set day will seek and take PCR
+                  will_take_PCR(student) = day + day_of_PCR(student);
+             end   
+
+
+
+         end
+     elseif Ext_isolate > 0
+        if r_infext(ii) < Ext_isolate
+        % if rand  < Ext_isolate
+            histInfnextday(student) = 1;
+            histDayInf(student) = day;
+            histRday(1,day) = histRday(1,day)+1;
+
+            histExtorIntnextday(student) = 1;
+
+           if Symptomatic(student)
+              %set day will seek and take PCR
+              will_take_PCR(student) = day + day_of_PCR(student);
+           end                          
+         end
+     end
+end
