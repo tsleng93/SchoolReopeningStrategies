220ac4179544353fdc7ae7f9694b7fd56da89b19
tsleng93 <51326999+tsleng93@users.noreply.github.com>
2022-01-24 16:45:11 +0000

    Update Moremodeloutputs.m

--- Moremodeloutputs.m
+++ Moremodeloutputs.m
@@ -1,4 +1,4 @@
-function [Incidence, Num_lat, Num_PCR, Num_conf, School_incidence] = Moremodeloutputs(history)
+function [Incidence, Num_lat, Num_PCR, Num_conf, School_incidence, Num_lat_t] = Moremodeloutputs(history)
 %This function extracts useful quantities from the history struct outputted
 %by interactingyeargroups.m
 
@@ -9,11 +9,11 @@ function [Incidence, Num_lat, Num_PCR, Num_conf, School_incidence] = Moremodelou
 %   - Outputs are described next to their introduction
 
 %Authors: Trystan Leng and Edward M. Hill 
-%Last update 05/02/2021.
+%Last update 07/12/2021.
 
 Weeks = 8;
-YearSize = 200;
-YearGroup = 5;
+YearSize = size(history.Infection,2);
+YearGroup = size(history.Infection,1);
 
 for day = 1:(Weeks*7)
     %True prevalence through time
@@ -27,6 +27,11 @@ Not_isolating = reshape(Not_isolating, YearGroup, length(Schooldaysandinitial));
 %Number of LFTs N.B. this only applies for strategies involving mass
 %testing and for all individuals opting into testing
 Num_lat = sum(sum(history.Test_days(:, Schooldaysandinitial).*Not_isolating));
+
+%Number of lateral flows through time
+Num_lat_t = zeros(1,Weeks*7);
+Num_lat_t(Schooldaysandinitial) = (sum(history.Test_days(:, Schooldaysandinitial).*Not_isolating));
+
 %Number of PCR tests
 Num_PCR = sum(sum(history.ever_test_pos == 2));
 %Number of confirmatory PCR tests (the same as the number of positive LFTs)
@@ -35,3 +40,4 @@ Num_conf = sum(sum(history.ever_test_pos == 1));
 
 %Within school incidence on last day of term
 School_incidence = sum(sum(history.ext_or_int(:,:, 54) == 2));
+
